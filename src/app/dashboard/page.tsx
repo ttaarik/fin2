120191be096d3cd7a5dd0cf28180ccad0e708a57
Tarik Kadric <kadric.tarik@hotmail.com>
2024-08-11 01:03:16 +0200

    Profile Page and SQL Tables added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -300,8 +300,7 @@ export default function Dashboard() {
         <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
           <Card
             className="xl:col-span-2" x-chunk="dashboard-01-chunk-4"
-          >
-            
+          > 
             <CardHeader className="flex flex-row items-center">
               <div className="grid gap-2">
                 <CardTitle>Transactions</CardTitle>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import { LoginField } from "@/components/ui/loginfield"
-// import "./src/app/globals.css";
+
 
 export default function LoginPage() {
   return (
